228aeb4928373d5a26e5044a76e44a269a874aca
Henryl21 <punayhenryl17@gmail.com>
2025-10-24 13:17:06 +0800

    dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7e8bdb7a1f398dbc7d04ebdf01b49c719b328c62',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -395,9 +395,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7e8bdb7a1f398dbc7d04ebdf01b49c719b328c62',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
